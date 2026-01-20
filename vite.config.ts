b3cb2ffcd7913f8afabcabc0992aa5b282c5a646
tentacioPro <maharajanabishek@gmail.com>
2026-01-20 07:47:36 +0530

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/externalBrain/', // GitHub Pages repository path
+  base: '/', // Use '/' for custom domain
 })
